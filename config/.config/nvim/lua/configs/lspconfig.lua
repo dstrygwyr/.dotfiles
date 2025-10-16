355c10e00dc7c4f2264711641df3e6f29551cf97
strygwyr <13832826+dstrygwyr@users.noreply.github.com>
2025-10-17 00:04:57 +0700

    chore: update lsp

--- config/.config/nvim/lua/configs/lspconfig.lua
+++ config/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,41 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+-- List of language servers to enable
+local servers = {
+  "html",           -- HTML
+  "cssls",          -- CSS
+  "ts_ls",          -- TypeScript/JavaScript (typescript-language-server)
+  "tailwindcss",    -- Tailwind CSS
+  "gopls",          -- Go
+  "rust_analyzer",  -- Rust
+  "lua_ls",         -- Lua
+  "intelephense",   -- PHP
+  "zls",            -- Zig
+  "emmet_ls",       -- Emmet
+  "dockerls",       -- Docker
+  "docker_compose_language_service", -- Docker Compose
+}
+
+-- Enable all language servers
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- Optional: Configure specific servers
+vim.lsp.config.lua_ls = {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          vim.fn.expand("$VIMRUNTIME/lua"),
+          vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+          vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+}
+
