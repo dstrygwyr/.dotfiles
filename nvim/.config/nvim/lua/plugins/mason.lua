846c99ff9ac51eedbf026ea55537b83734d564cf
strygwyr <13832826+dstrygwyr@users.noreply.github.com>
2025-10-23 17:38:42 +0700

    chore: default lsp

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -5,6 +5,35 @@ return {
       ui = {
         border = "rounded",
       },
+      ensure_installed = {
+        -- LSP Servers
+        "lua-language-server",
+        "typescript-language-server",
+        "html-lsp",
+        "css-lsp",
+        "json-lsp",
+        "tailwindcss-language-server",
+        "vue-language-server",
+        "rust-analyzer",
+        "python-lsp-server",
+        "intelephense",
+        "prisma-language-server",
+        "yaml-language-server",
+        "docker-language-server",
+        "docker-compose-language-service",
+        "zls",
+        
+        -- Formatters & Linters
+        "stylua",
+        "shfmt",
+        "htmlhint",
+        "sql-formatter",
+        "biome",
+        
+        -- Tools
+        "tree-sitter-cli",
+        "postgrestools",
+      },
     },
   },
 
